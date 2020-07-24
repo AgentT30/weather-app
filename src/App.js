dc7171f33f7c45c334294feae63f918dcb041294
AgentT30 <cthekkunja304@gmail.com>
2020-07-24 20:41:29 +0530

    Dynamic Weather Updated

--- src/App.js
+++ src/App.js
@@ -1,56 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 
 const apiKeys = {
-    key: "bccf8fe7f752ac28bdc64bdcdba6922d",
-    base: "https://api.openweathermap.org/data/2.5/",
+  key: "bccf8fe7f752ac28bdc64bdcdba6922d",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
-    const dateBuilder = (d) => {
-        let months = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-        let day = days[d.getDay()];
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-        let date = d.getDate();
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(
+        `${apiKeys.base}weather?q=${query}&units=metric&APPID=${apiKeys.key}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
 
-        return `${day} ${month} ${year} ${date}`;
-    };
-    return (
-        <div className="container">
-            <main>
-                <div className="search-box">
-                    <input
-                        type="text"
-                        className="searchfield"
-                        placeholder="Enter City Name"
-                    ></input>
-                </div>
-                <div className="location-box">
-                    <div className="location">NY City</div>
-                    <div className="date">{dateBuilder(new Date())}</div>
-                </div>
-                <div className="weather-box">
-                    <div className="temperature">20° C</div>
-                    <div className="weather">Sunny</div>
-                </div>
-            </main>
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let date = d.getDate();
+
+    return `${day}, ${date} ${month} ${year} `;
+  };
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 20
+            ? "container warm"
+            : "container"
+          : "container clear"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="searchfield"
+            placeholder="Enter City Name"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          ></input>
         </div>
-    );
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          <div className="opening-page">
+            <p>Please enter a name of a city to get the weather details.</p>
+          </div>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
